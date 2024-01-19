e459d6764e225b935555f1d25f43a265e95974e9
Tob Stoic <rafaeltobass@gmail.com>
2024-01-19 06:01:23 -0500

    Primer commit

---
+++ src/FirstApp.jsx
@@ -0,0 +1,15 @@
+
+function FirstApp() {
+ 
+ 
+    return (
+        <>
+            <h1>  First App </h1>
+            <p>Soy un subtitulo</p>
+        </>
+  
+    )
+  
+  }
+
+  export default FirstApp
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import  App  from './HelloWorldApp'
+import FirstApp from './FirstApp'
+
+
+
+
+ReactDOM.createRoot( document.getElementById('root')).render(
+  <React.StrictMode>
+    <App></App>
+    <FirstApp></FirstApp>
+  </React.StrictMode>
+)
